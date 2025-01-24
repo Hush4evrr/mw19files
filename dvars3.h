ff5110068f776c94bcb1ba74f41490849ecc6698
Hush4evrr <billybobjoeljr35@outlook.com>
2025-01-24 02:21:34 -0500

    Create dvars3.h

---
+++ dvars3.h
@@ -0,0 +1,18 @@
+Cbuf_AddText("LTOKRMRTMM 0"); // no fog
+Cbuf_AddText("NLPSQMQRTP 1"); // old mw text box host only 
+Cbuf_AddText("MNKLKSPRT 10 "); // MW2 wallhack on
+Cbuf_AddText("xstartlobby"); // start lobby 1
+Cbuf_AddText("xstopparty;xstopprivateparty"); // end party
+Cbuf_AddText("xpartygo"); // start lobby 2
+Cbuf_AddText("LQKPQMPRQN 1;LLSMRRKRMK 9999;NTTNORKMKT 9999;OLKRNORMOQ 1;LTLQSKRKQM 1;QKMSSSLPK 1;SKNSKQTQR 1;MRMLMLLMMP 1;"); //hear everyone dead or not. might have been patched
+Cbuf_AddText("PPLNNTNTP 1");// colorblind mode
+Cbuf_AddText("NTNKLMLTLS 999;NPTMROTQNQ 999;SRTTSMPNO 999;MQRTTRRSOK 999;MQQNRSLRNS 999");// perk extra breath , might not work anymore.
+Cbuf_AddText("SROLTPQLK 0");// no gun
+Cbuf_AddText("SROLTPQLK 1");// gun show 
+Cbuf_AddText("set NSSLSNKPN 2.0"); // fov 220
+Cbuf_AddText("LSSRRSMNMR 1"); // dev playlist
+Cbuf_AddText("hostmigration_start"); // kick your self
+Cbuf_AddText("MKMRRLNRTM 5"); // huge map size 1-10
+Cbuf_AddText("OPOTTRRNQ 0;NNRQTQNLRL 0;NNTOQLMSLO 3;NTROOKOSPM 3;NNKRMTSNPL 0;MOLLKMTMRS 0;MQOPRPQLQL 0;NLNKQNROQM 0;LTOKRMRTMM 0");// fps boost
+Cbuf_AddText("MMSRPNPNON 999; NRMSPRNPMQ 999");// glitch prone host only  view goes in the sky !
+Cbuf_AddText("LLLRKRROOP 1"); // mess up your view up
